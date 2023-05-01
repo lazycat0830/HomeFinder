940968ac0e6925868d1c6587c75a58ed43922f53
lazycat0830 <9700865@gmail.com>
2023-05-02 06:24:30 +0800

    register

--- javascript/API/Members/register.js
+++ javascript/API/Members/register.js
@@ -10,10 +10,8 @@ submit.addEventListener("click", (event) => {
   // validatatext_signup.innerHTML='';
   const formData = new FormData(form);
   
-  if(formData.get("password")!=formData.get("passwordCheck")){
-    validatatext_signup.innerHTML='密碼不一致';
-    validatatext_signup.style.display='block';
-  }else{
+  
+   
     console.log(formData.get("newMember.identity"));
 
     formData.append('newMember.authcode','0');
@@ -21,28 +19,61 @@ submit.addEventListener("click", (event) => {
     formData.append('newMember.img','0');
     console.log(formData.get("ProfileImage"));
     // if(formData.get(ProfileImage)==null)
-  
-    axios({
-      method: "post",
-      url: "http://localhost:5190/api/Auth/register",
-      headers: {
-        "Content-Type": "multipart/form-data",
-        'Accept': "application/json",
-        // Authorization: `Bearer ${token}`,
-      },
-      data: formData,
-    })
-      .then((response) => {
-        console.log(response.data);
-        
-        validatatext_signup.innerHTML=response.data.message;
+    
+    console.log(formData.get("newMember.phone").length>10);
+    if(formData.get("newMember.identity")==null){
+      validatatext_signup.innerHTML='請選擇身分';
+    }else if(formData.get("newMember.name")==""){
+      validatatext_signup.innerHTML='名字為必填';
+    }else if(formData.get("newMember.account")==""){
+      validatatext_signup.innerHTML='帳號為必填';
+    }else if(formData.get("password")==""){
+      validatatext_signup.innerHTML='密碼為必填';
+    }else if(formData.get("passwordCheck")==""){
+      validatatext_signup.innerHTML='確認密碼為必填';
+    }else if(formData.get("password")!=formData.get("passwordCheck")){
+      validatatext_signup.innerHTML='密碼不一致';
+    }else if(formData.get("newMember.phone")==""){
+      validatatext_signup.innerHTML='電話為必填';
+    }else if(formData.get("newMember.phone").length>10){
+      validatatext_signup.innerHTML='電話為10字元';
+    }else if(formData.get("newMember.email")==""){
+      validatatext_signup.innerHTML='email為必填';
+    }else if(formData.get("ProfileImage").name==""){
+      validatatext_signup.innerHTML='請上傳圖片';
+    }else if(!formData.get("newMember.email").includes('@')){
+      validatatext_signup.innerHTML='請填寫正確的Email';
+    }else{
+      axios({
+        method: "post",
+        url: "http://localhost:5190/api/Auth/register",
+        headers: {
+          "Content-Type": "multipart/form-data",
+          'Accept': "application/json",
+          // Authorization: `Bearer ${token}`,
+        },
+        data: formData,
       })
-      .catch((error) => {
-        console.error(error);
-        // validatatext_signup.innerHTML='全部必填';
-        // validatatext_signup.style.display='block';
-      });
-  }
+        .then((response) => {
+          console.log(response.data);
+          if(response.data=='此帳號已被註冊'){
+            validatatext_signup.innerHTML='此帳號已被註冊';
+          }else if(response.data.message){
+            validatatext_signup.innerHTML=response.data.message;
+          }
+          
+          
   
-});
+        })
+        .catch((error) => {
+          console.error(error);
+          // validatatext_signup.innerHTML='全部必填';
+          // validatatext_signup.style.display='block';
+        });
+    }
+    }
+
+    
+  
+);
 
