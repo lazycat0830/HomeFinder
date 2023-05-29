a6fbe0e3c5fd9fed528e98a10f59c4c0627c2970
lazycat0830 <9700865@gmail.com>
2023-05-30 01:42:04 +0800

    updata

--- javascript/API/Members/time_management.js
+++ javascript/API/Members/time_management.js
@@ -56,7 +56,7 @@ deleteX_btn.onclick=function(){
 }
 
 function viewtime_management(changedate){
-    getDataTimeinput(changedate);
+    // getDataTimeinput(changedate);
     item_content.innerHTML=``;
     validataText_time.style.display='none';
     axios({
@@ -110,121 +110,121 @@ function viewtime_management(changedate){
     });
 }
 
-function getDataTimeinput(changedate){
-    console.log('BookOfDay date:',changedate.replace(/\//g, '-').replace(/\b(\d)\b/g, '0$1'));
-    console.log('BookOfDay account:',LoginData.members.account);
+// function getDataTimeinput(changedate){
+//     console.log('BookOfDay date:',changedate.replace(/\//g, '-').replace(/\b(\d)\b/g, '0$1'));
+//     console.log('BookOfDay account:',LoginData.members.account);
 
-    let formData=new FormData();
-    formData.append('date',changedate.replace(/\//g, '-').replace(/\b(\d)\b/g, '0$1'));
-    formData.append('account',LoginData.members.account);
+//     let formData=new FormData();
+//     formData.append('date',changedate.replace(/\//g, '-').replace(/\b(\d)\b/g, '0$1'));
+//     formData.append('account',LoginData.members.account);
 
-    axios({
-        method:'post',
-        url:'http://localhost:5190/api/Time/BookOfDayTool',
-        headers:{
-            "Content-Type": "multipart/form-data",
-            "Accept": "application/json",
-            "Authorization": `Bearer ${LoginData.token}`,
-        },data:formData,
-    }).then(({ data })=> {
-        let Edittime=document.getElementById('Edittime');
-        for(var i=0;i<7;i++){
-            let addgetTime=document.createElement('li');
-            addgetTime.id=`Timeli${i}`;
-            addgetTime.innerHTML=`
-            <div class="flexcolumn">
-                <label id="start_time${i}" style="font-size: 16px;text-align: center;">--:--</label>
-                <label id="end_time${i}" style="font-size: 16px;text-align: center;">--:--</label>
-                <input style="margin: 0px 20px;" id="Editbtn_time${i}" type="button"  value="修改">
-            </div>
-            `;
-        Edittime.appendChild(addgetTime);
+//     axios({
+//         method:'post',
+//         url:'http://localhost:5190/api/Time/BookOfDayTool',
+//         headers:{
+//             "Content-Type": "multipart/form-data",
+//             "Accept": "application/json",
+//             "Authorization": `Bearer ${LoginData.token}`,
+//         },data:formData,
+//     }).then(({ data })=> {
+//         let Edittime=document.getElementById('Edittime');
+//         for(var i=0;i<7;i++){
+//             let addgetTime=document.createElement('li');
+//             addgetTime.id=`Timeli${i}`;
+//             addgetTime.innerHTML=`
+//             <div class="flexcolumn">
+//                 <label id="start_time${i}" style="font-size: 16px;text-align: center;">--:--</label>
+//                 <label id="end_time${i}" style="font-size: 16px;text-align: center;">--:--</label>
+//                 <input style="margin: 0px 20px;" id="Editbtn_time${i}" type="button"  value="修改">
+//             </div>
+//             `;
+//         Edittime.appendChild(addgetTime);
 
-            let EditTimebtn=document.getElementById(`Editbtn_time${i}`);
-            EditTimebtn.onclick=function(event){
-                console.log(event.target);
-                addgetTime.innerHTML=`
-                <div class="flexcolumn">
-                    <input id="start_time${event.target.id.replace('Editbtn_time','')}" type='time' style="font-size: 16px;text-align: center;"/>
-                    <input id="end_time${event.target.id.replace('Editbtn_time','')}" type='time' style="font-size: 16px;text-align: center;"/>
-                    <input style="margin: 0px 20px;" id="Savebtn_time${event.target.id.replace('Editbtn_time','')}" onclick='SaveTimebtn(event,${changedate})' type="button"  value="完成">
-                    <input style="margin: 0px 20px;" id="Clearbtn_time${event.target.id.replace('Editbtn_time','')}" onclick='ClearTimebtn(event,${changedate})' type="button"  value="清除">
-                </div>
-                `;
-                let list_onetime=data.availableTimesArray[event.target.id.replace('Editbtn_time','')].split('-');
-                let start_time=document.getElementById(`start_time${event.target.id.replace('Editbtn_time','')}`);
-                let end_time=document.getElementById(`end_time${event.target.id.replace('Editbtn_time','')}`);
-                start_time.value=list_onetime[0];
-                end_time.value=list_onetime[1];
-            }
-        }
-        console.log(data.availableTimesArray);
-        var id=0;
-        data.availableTimesArray.forEach(function(){
-            if(data.availableTimesArray[id]!=""){
-                let list_onetime=data.availableTimesArray[id].split('-');
-                let start_time=document.getElementById(`start_time${id}`);
-                let end_time=document.getElementById(`end_time${id}`);
-                start_time.innerHTML=list_onetime[0];
-                end_time.innerHTML=list_onetime[1];
+//             let EditTimebtn=document.getElementById(`Editbtn_time${i}`);
+//             EditTimebtn.onclick=function(event){
+//                 console.log(event.target);
+//                 addgetTime.innerHTML=`
+//                 <div class="flexcolumn">
+//                     <input id="start_time${event.target.id.replace('Editbtn_time','')}" type='time' style="font-size: 16px;text-align: center;"/>
+//                     <input id="end_time${event.target.id.replace('Editbtn_time','')}" type='time' style="font-size: 16px;text-align: center;"/>
+//                     <input style="margin: 0px 20px;" id="Savebtn_time${event.target.id.replace('Editbtn_time','')}" onclick='SaveTimebtn(event,${changedate})' type="button"  value="完成">
+//                     <input style="margin: 0px 20px;" id="Clearbtn_time${event.target.id.replace('Editbtn_time','')}" onclick='ClearTimebtn(event,${changedate})' type="button"  value="清除">
+//                 </div>
+//                 `;
+//                 let list_onetime=data.availableTimesArray[event.target.id.replace('Editbtn_time','')].split('-');
+//                 let start_time=document.getElementById(`start_time${event.target.id.replace('Editbtn_time','')}`);
+//                 let end_time=document.getElementById(`end_time${event.target.id.replace('Editbtn_time','')}`);
+//                 start_time.value=list_onetime[0];
+//                 end_time.value=list_onetime[1];
+//             }
+//         }
+//         console.log(data.availableTimesArray);
+//         var id=0;
+//         data.availableTimesArray.forEach(function(){
+//             if(data.availableTimesArray[id]!=""){
+//                 let list_onetime=data.availableTimesArray[id].split('-');
+//                 let start_time=document.getElementById(`start_time${id}`);
+//                 let end_time=document.getElementById(`end_time${id}`);
+//                 start_time.innerHTML=list_onetime[0];
+//                 end_time.innerHTML=list_onetime[1];
                 
-            }
-            id++;
-        });
+//             }
+//             id++;
+//         });
         
 
 
-    //     if(monlist!=""){
-    //         monlist.forEach(function(){
-    //        console.log(monlist[i]);
-    //        let monlist_onetime=monlist[i].split('-');
-    //        console.log(monlist_onetime);
-    //        let monup=document.getElementById(`monup${i+1}`);
-    //        monup.value=monlist_onetime[0];
-    //        let mondown=document.getElementById(`mondown${i+1}`);
-    //        mondown.value=monlist_onetime[1];
-    //        i++;
-    //    });
+//     //     if(monlist!=""){
+//     //         monlist.forEach(function(){
+//     //        console.log(monlist[i]);
+//     //        let monlist_onetime=monlist[i].split('-');
+//     //        console.log(monlist_onetime);
+//     //        let monup=document.getElementById(`monup${i+1}`);
+//     //        monup.value=monlist_onetime[0];
+//     //        let mondown=document.getElementById(`mondown${i+1}`);
+//     //        mondown.value=monlist_onetime[1];
+//     //        i++;
+//     //    });
 
-    }).catch(error=>{
-        console.log(error);
-    })
-}
+//     }).catch(error=>{
+//         console.log(error);
+//     })
+// }
 
 
-function postoneTime(){
+// function postoneTime(){
     
     
-}
+// }
 
-function SaveTimebtn(event,changedate){
-    console.log(event.target.id);
-    var id=event.target.id.replace('Savebtn_time','');
-    let data;
-    for(var i=0;i<6;i++){
-       let start=document.getElementById(`start_time${i}`);
-       let end=document.getElementById(`end_time${i}`);
-       data+=start+'-'+end+',';
+// function SaveTimebtn(event,changedate){
+//     console.log(event.target.id);
+//     var id=event.target.id.replace('Savebtn_time','');
+//     let data;
+//     for(var i=0;i<6;i++){
+//        let start=document.getElementById(`start_time${i}`);
+//        let end=document.getElementById(`end_time${i}`);
+//        data+=start+'-'+end+',';
     
-    }
-    console.log(changedate.replace(/\//g, '-').replace(/\b(\d)\b/g, '0$1'));
-    console.log(data);
-    axios({
-        method:'post',
-        url:'http://localhost:5190/api/Time/SetSpecialTime',
-        headers:{
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": `Bearer ${LoginData.token}`,
-        },data:{
-            date:changedate.replace(/\//g, '-').replace(/\b(\d)\b/g, '0$1'),
-            newtime:'',
-        },
-    }).then(({ data })=> {
-        console.log(data);
+//     }
+//     console.log(changedate.replace(/\//g, '-').replace(/\b(\d)\b/g, '0$1'));
+//     console.log(data);
+//     axios({
+//         method:'post',
+//         url:'http://localhost:5190/api/Time/SetSpecialTime',
+//         headers:{
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//             "Authorization": `Bearer ${LoginData.token}`,
+//         },data:{
+//             date:changedate.replace(/\//g, '-').replace(/\b(\d)\b/g, '0$1'),
+//             newtime:'',
+//         },
+//     }).then(({ data })=> {
+//         console.log(data);
         
         
-    }).catch(error=>{
-        console.log(error);
-    })
-}
+//     }).catch(error=>{
+//         console.log(error);
+//     })
+// }
